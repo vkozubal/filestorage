0baa418cd4afc93de85da8994375334f5c01fcd0
kozv <volodumyr.kozubal@gmail.com>
2017-04-01 20:20:38 +0300

    added service interface

---
+++ file-storage-api/src/main/java/kozv/fs/api/service/IFileStorageService.java
@@ -0,0 +1,28 @@
+package kozv.fs.api.service;
+
+import kozv.fs.api.model.DataFile;
+import kozv.fs.api.model.FileAttributes;
+
+import java.util.List;
+
+public interface IFileStorageService {
+
+    /**
+     * Saves the given file which may contain data along with given file attributes and metadata
+     *
+     * @param file to store
+     * @return the saved entity
+     */
+    FileAttributes save(DataFile file);
+
+    /**
+     * @param id id must not be {@literal null}.
+     * @return an {@link FileAttributes} by its id, or {@literal null} if none found
+     */
+    DataFile findOne(String id) ;
+
+    /**
+     * @return all user files
+     */
+    List<FileAttributes> findAll();
+}
